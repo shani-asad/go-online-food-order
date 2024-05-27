13734fbdc1b3b6f74c781853b94194b62bc1b775
Farhan Ramadhana <farhanramadhana97@gmail.com>
2024-05-27 15:08:30 +0700

    add merchant timestamp

--- db/migrations/20240526130605_add_merchants_table.up.sql
+++ db/migrations/20240526130605_add_merchants_table.up.sql
@@ -4,5 +4,7 @@ CREATE TABLE merchants (
     merchant_category VARCHAR(20) NOT NULL CHECK (merchant_category IN ('SmallRestaurant', 'MediumRestaurant', 'LargeRestaurant', 'MerchandiseRestaurant', 'BoothKiosk', 'ConvenienceStore')),
     image_url TEXT NOT NULL,
     location_lat FLOAT NOT NULL,
-    location_long FLOAT NOT NULL
+    location_long FLOAT NOT NULL,
+    created_at timestamp default CURRENT_TIMESTAMP,
+    updated_at timestamp
 );
