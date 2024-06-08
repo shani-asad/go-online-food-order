de5f468b25e72095a56e62eb0f3d2a73a6f036de
shani <mshaniasadulhaq@gmail.com>
2024-06-08 13:15:03 +0700

    feat: update orders table migration, add already_placed column

--- db/migrations/20240608045149_add_orders_table.up.sql
+++ db/migrations/20240608045149_add_orders_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE orde (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     merchant_id INT NOT NULL,
+    already_placed BOOLEAN,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
